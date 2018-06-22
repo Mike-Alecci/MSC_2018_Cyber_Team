8627e58ad1e7415d8e5baa79d13089fbf793b827
Mike-Alecci <michaelalecci@gmail.com>
2018-06-22 14:36:17 -0400

    Some small updates

--- Identification_Script.py
+++ Identification_Script.py
@@ -1,19 +1,21 @@
 import csv
+import os
 
 class HackingDrones:
     """Trying to hack drones and cool stuff"""
-    def __init__(self, known_ips):
+    def __init__(self, known_ips, dirc = os.getcwd()):
         """The init method instantiates the known list of ips to track"""
         self.known_ips = known_ips
         self.matched_ips = dict()   #format for this dictionary is potential matches' {IP: [channel, name]}
         self.identification()
+        os.chdir(dirc)
         
 
-    def read_files(self, file_name, seperator = ", "):
+    def read_files(self, file_name, expected_num_values, seperator = ", "):
         """A generic read file generator to check bad file inputs and read line by line"""
         try:
             fp = open(file_name, 'rb') 
-        except FileNotFoundError:
+        except FileNotFoundError: 
             raise FileNotFoundError ("Could not open {}".format(file_name))
         else:
             with fp:
@@ -22,18 +24,19 @@ class HackingDrones:
                     if x > 1:
                         st_line = str(line)
                         l = st_line.strip().split(seperator)
-                        if len(l) == 14:
+                        if len(l) == expected_num_values:
                             yield l
                     x += 1
 
     def identification(self):
         """This function takes the information from the generator and then attempts to identify if a 
            returned IP is a known drone IP, it then compiles a dictionary of matched IP's"""
-        read_network_ips = self.read_files("output-01.csv", seperator = ", ")
-        for bssid, a, b, channel, d, e, f, g, h, i, j, k, name, l in read_network_ips:
+        read_network_ips = self.read_files("output-01.csv", 14, seperator = ", ")
+        for bssid, first_time_seen, last_time_seen, channel, speed, privacy, cipher, authentication,\
+            power, beacons, IV, LAN, name, end in read_network_ips:
             if bssid[2:10] in self.known_ips:
                 print("Match found at this IP: {}".format(bssid[2:]))
-                self.matched_ips[bssid[2:]] = [channel, name]
+                self.matched_ips[bssid[2:]] = [channel.strip(), name]
         print("Here are all the drone IP's: {}".format(self.matched_ips))
         return self.matched_ips
 
